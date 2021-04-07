b110203c2aaaeea3c9eea7b7c04baa8688c61248
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-04-07 14:51:32 +0200

    added movies into endpoint GET

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import Movies from "./movies.js";
 
-const products = [];
+const express = require('express');
+const app = Express();
+const port = 3000;
 
 //Parsing
 app.use(express.json());
 
 //Endpoints
-app.get('/api/products', (req, res) => {
-    res.json(products);
+app.get("/api/movies/:id", (req, res) => {
+    res.json(Movies.find((movie) => {
+        return +req.params.id === movie.id;
+    }))
+    res.send(req.params.id)
 });
 
 //Starting the server
