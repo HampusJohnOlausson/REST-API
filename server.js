5a1cbf0e9f7e3820154cf4d8ff7cd86f1609317b
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-04-08 09:34:45 +0200

    changed path and added info to server.js

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
-import movies from "../public/movies.js";
+
 import express from "express";
 
 const app = express();
 const port = 3000;
+
+const movies = [
+    {
+        id: 1,
+        title: 'Pans Labyrinth',
+        Director: 'Guillermo Del Toro'
+    },
+    {
+        id: 2,
+        title: 'Blue Valentine',
+        director: 'Derek Cianfrance'
+    },
+    {
+        id: 3,
+        title: 'Gladiator',
+        director: 'Ridley Scott'
+    },
+    {
+        id: 4,
+        title: 'Eternal sunshine of the spotless mind',
+        director: 'Michel Gondry'
+    }
+];
 
 //Parsing
 app.use(express.json());
 
 //Endpoints
 app.get("/api/movies/:id", (req, res) => {
-    res.json(movies.find((movie) => {
-        return req.params.id === movie.id;
-    }))
+    res.send(movies)
 });
 
 //Starting the server
